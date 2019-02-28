7e585430e9166cd142152bbcff1996c25f5ec344
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-02-28 09:26:47 -0800

    gatsby portfolio project

---
+++ hello-world/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => (
+  <div style={{ color: `purple` }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="random" />
+  </div>
+)
+
+export default Home
